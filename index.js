4b31adc57a27cd6a0dc59350e3821b0ada159b6a
Sean <oseanwave@gmail.com>
2023-03-07 01:47:18 -0500

    commit tree 3211b0b4588f32555640ff62409e40cd69c79705

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Number.parseInt(Math.random() * 10 + 1);
+
+const num3 = 60;
+const num4 = 7;
+const mod = 60 % 7;
+
+const max = Math.max(2, 4, 5, 8, 20);
